49172c5aad6d22766d5bcafb37a35b3c55905281
otekolste <58699635+otekolste@users.noreply.github.com>
2024-10-27 19:40:55 -0400

    added add_user and login_user mutations and resolvers

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -35,3 +35,27 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addProfile(username: $username, email: $email, password: $password) {
+      token
+      profile {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        username
+      }
+    }
+  }
+`;
